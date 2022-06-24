e61bc56c7d6037f93e3637a7b275245b39a1ce6e
Hugo Arregui <hugo@decentraland.org>
2022-06-24 15:21:14 -0300

    use esm modules

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,18 @@
 module.exports = {
+  extensionsToTreatAsEsm: ['.ts'],
   globals: {
     'ts-jest': {
       tsconfig: 'test/tsconfig.json',
+      'useESM': true
     },
   },
   moduleFileExtensions: ['ts', 'js'],
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(ts|tsx)$': 'ts-jest'
   },
   testTimeout: 60000,
   coverageDirectory: 'coverage',
   verbose: true,
   testMatch: ['**/*.spec.(ts)'],
   testEnvironment: 'node',
-  "transformIgnorePatterns": [
-    "/node_modules/"
-  ],
 }
