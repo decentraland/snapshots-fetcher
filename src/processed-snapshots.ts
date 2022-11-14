97a72f72b3fc4c55392b28c3e7f532da604d330f
Pedro Tamborindeguy <pftambo@gmail.com>
2022-11-14 13:12:16 -0300

    chore: add comments for the processedSnapshots component

--- src/processed-snapshots.ts
+++ src/processed-snapshots.ts
@@ -2,6 +2,21 @@ import { IProcessedSnapshotsComponent, SnapshotsFetcherComponents } from "./type
 
 /**
  * Creates the component that signals the streaming and processing of a snapshot.
+ * It decides wheter or not a snapshot should be streamed when syncing. In _index.ts_ the snapshots are fetched from
+ * /snapshots or /snapshot. For each one of those, this component will be used to decide to stream the entities inside
+ * it or not. It will return false if the snapshot was already processed or if it's currently being streamed in other
+ * execution. Once the streaming starts, it will be signalized @startStreamOf so this component knows that a snapshot is
+ * being currently streamed.
+ *
+ * Each entity streamed within a snapshot will be processed by the catalyst/batchDeployer. It adds each one to a queue
+ * and they are processed in background. Once a deploy in background finishes, it signals this component that one
+ * entity from the snapshot was processed using @entityProcessedFrom.
+ *
+ * When the stream of all deployments it's done within a snapshot, then the signal @endStreamOf is called together with
+ * the quantity of entities streamed. If the quantity matches the number of @entityProcessedFrom signals, it saves
+ * the snapshot as processed. If not, every time the signal @entityProcessedFrom is called, it will check if this
+ * condition is met in order to save or not the snapshot as procesed.
+ *
  *
  * @public
  */
