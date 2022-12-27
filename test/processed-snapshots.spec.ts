09bc8fcbd3188b92db02e57e82f0ff86c3bd1cc9
Pedro Tamborindeguy <pftambo@gmail.com>
2022-12-27 18:13:09 -0300

    test: add processed snapshots tests

--- test/processed-snapshots.spec.ts
+++ test/processed-snapshots.spec.ts
@@ -9,7 +9,7 @@ import { metricsDefinitions } from '../src'
 
 describe('processed snapshots', () => {
 
-  const processedSnapshot = 'someHash'
+  const processedSnapshotHash = 'someHash'
   const h1 = 'h1'
   const h2 = 'h2'
   const h3 = 'h3'
@@ -33,97 +33,101 @@ describe('processed snapshots', () => {
     processedSnapshots = createProcessedSnapshotsComponent({ processedSnapshotStorage, logs, metrics })
   })
 
-  // describe('shouldStream', () => {
-  //   it('should return false when the snapshot was processed', async () => {
-  //     processedSnapshotStorage.saveProcessed(processedSnapshot)
-  //     expect(await processedSnapshots.shouldStream(processedSnapshot)).toBeFalsy()
-  //   })
+  describe('shouldStream', () => {
+    it('should return false when the snapshot was processed', async () => {
+      processedSnapshotStorage.saveProcessed(processedSnapshotHash)
+      expect(await processedSnapshots.shouldProcessSnapshot(processedSnapshotHash, [])).toBeFalsy()
+    })
 
-  //   it('should return false when the replaced hashes where processed', async () => {
-  //     processedSnapshotStorage.saveProcessed(h1)
-  //     processedSnapshotStorage.saveProcessed(h2)
-  //     processedSnapshotStorage.saveProcessed(h3)
+    it('should return false when the replaced hashes where processed (in the same group)', async () => {
+      processedSnapshotStorage.saveProcessed(h1)
+      processedSnapshotStorage.saveProcessed(h2)
+      processedSnapshotStorage.saveProcessed(h3)
 
-  //     expect(await processedSnapshots.shouldStream(processedSnapshot, [h1, h2, h3])).toBeFalsy()
-  //   })
+      expect(await processedSnapshots.shouldProcessSnapshot(processedSnapshotHash, [[h1, h2, h3]])).toBeFalsy()
+    })
 
-  //   it('should return false and save the snapshot as processed when the replaced hashes where processed', async () => {
-  //     processedSnapshotStorage.saveProcessed(h1)
-  //     processedSnapshotStorage.saveProcessed(h2)
-  //     processedSnapshotStorage.saveProcessed(h3)
+    it('should save the snapshot as processed when all the hashes of any replaced-hashes group were processed', async () => {
+      processedSnapshotStorage.saveProcessed(h1)
+      processedSnapshotStorage.saveProcessed(h2)
+      processedSnapshotStorage.saveProcessed(h3)
 
-  //     await processedSnapshots.shouldStream(processedSnapshot, [h1, h2, h3])
+      expect(await processedSnapshots.shouldProcessSnapshot(processedSnapshotHash, [[h1, h2, h3], ['non-processed']])).toBeFalsy()
+      const processed = await processedSnapshotStorage.processedFrom([processedSnapshotHash])
+      expect(processed.has(processedSnapshotHash)).toBeTruthy()
+    })
 
-  //     const processedSnapshotsInDB = await processedSnapshotStorage.processedFrom([processedSnapshot])
-  //     expect(processedSnapshotsInDB.has(processedSnapshot)).toBeTruthy()
-  //   })
+    it('should return true when not all the replaced hashes of any group were processed', async () => {
+      processedSnapshotStorage.saveProcessed(h1)
+      processedSnapshotStorage.saveProcessed(h2)
+      processedSnapshotStorage.saveProcessed(h3)
 
-  //   it('should return false when the snapshot is being streamed', async () => {
-  //     processedSnapshots.startStreamOf(processedSnapshot)
-  //     expect(await processedSnapshots.shouldStream(processedSnapshot)).toBeFalsy()
-  //   })
+      expect(await processedSnapshots.shouldProcessSnapshot(processedSnapshotHash, [[h1, h2, 'non-processed'], [h3, 'non-processed']])).toBeTruthy()
+      const processed = await processedSnapshotStorage.processedFrom([processedSnapshotHash, 'non-processed'])
+      expect(processed.size).toEqual(0)
+    })
 
-  //   it('should return false when the snapshot was completely streamed', async () => {
-  //     processedSnapshots.startStreamOf(processedSnapshot)
-  //     processedSnapshots.endStreamOf(processedSnapshot, 1)
-  //     expect(await processedSnapshots.shouldStream(processedSnapshot)).toBeFalsy()
-  //   })
+    it('should return false when the snapshot is being streamed', async () => {
+      processedSnapshots.startStreamOf(processedSnapshotHash)
+      expect(await processedSnapshots.shouldProcessSnapshot(processedSnapshotHash, [])).toBeFalsy()
+    })
 
-  //   it('should return true when the snapshot and the replaced ones were not processed', async () => {
-  //     expect(await processedSnapshots.shouldStream(processedSnapshot, [h1, h2, h3])).toBeTruthy()
-  //   })
-
-  // })
+    it('should return false when the snapshot was completely streamed', async () => {
+      processedSnapshots.startStreamOf(processedSnapshotHash)
+      processedSnapshots.endStreamOf(processedSnapshotHash, 0)
+      expect(await processedSnapshots.shouldProcessSnapshot(processedSnapshotHash, [])).toBeFalsy()
+    })
+  })
 
   describe('endStreamOf', () => {
     it('should not save the snapshot as processed when there are still entities to process', async () => {
-      processedSnapshots.startStreamOf(processedSnapshot)
-      processedSnapshots.endStreamOf(processedSnapshot, 1)
+      processedSnapshots.startStreamOf(processedSnapshotHash)
+      processedSnapshots.endStreamOf(processedSnapshotHash, 1)
 
-      const processedSnapshotsInDB = await processedSnapshotStorage.processedFrom([processedSnapshot])
+      const processedSnapshotsInDB = await processedSnapshotStorage.processedFrom([processedSnapshotHash])
       expect(processedSnapshotsInDB.size).toEqual(0)
     })
 
     it('should save the snapshot as processed when there are not entities to process', async () => {
-      processedSnapshots.startStreamOf(processedSnapshot)
-      processedSnapshots.entityProcessedFrom(processedSnapshot)
-      processedSnapshots.endStreamOf(processedSnapshot, 1)
+      processedSnapshots.startStreamOf(processedSnapshotHash)
+      processedSnapshots.entityProcessedFrom(processedSnapshotHash)
+      processedSnapshots.endStreamOf(processedSnapshotHash, 1)
 
-      const processedSnapshotsInDB = await processedSnapshotStorage.processedFrom([processedSnapshot])
+      const processedSnapshotsInDB = await processedSnapshotStorage.processedFrom([processedSnapshotHash])
       expect(processedSnapshotsInDB.size).toEqual(1)
     })
   })
 
   describe('entityProcessedFrom', () => {
     it('should not save the snapshot as processed when the stream is not finished yet', async () => {
-      processedSnapshots.startStreamOf(processedSnapshot)
-      processedSnapshots.entityProcessedFrom(processedSnapshot)
-      processedSnapshots.entityProcessedFrom(processedSnapshot)
+      processedSnapshots.startStreamOf(processedSnapshotHash)
+      processedSnapshots.entityProcessedFrom(processedSnapshotHash)
+      processedSnapshots.entityProcessedFrom(processedSnapshotHash)
 
-      const processedSnapshotsInDB = await processedSnapshotStorage.processedFrom([processedSnapshot])
+      const processedSnapshotsInDB = await processedSnapshotStorage.processedFrom([processedSnapshotHash])
       expect(processedSnapshotsInDB.size).toEqual(0)
     })
 
     it('should not save the snapshot as processed when the stream is finished but there still are entities to process', async () => {
-      processedSnapshots.startStreamOf(processedSnapshot)
-      processedSnapshots.endStreamOf(processedSnapshot, 3)
-      processedSnapshots.entityProcessedFrom(processedSnapshot)
-      processedSnapshots.entityProcessedFrom(processedSnapshot)
+      processedSnapshots.startStreamOf(processedSnapshotHash)
+      processedSnapshots.endStreamOf(processedSnapshotHash, 3)
+      processedSnapshots.entityProcessedFrom(processedSnapshotHash)
+      processedSnapshots.entityProcessedFrom(processedSnapshotHash)
 
 
-      const processedSnapshotsInDB = await processedSnapshotStorage.processedFrom([processedSnapshot])
+      const processedSnapshotsInDB = await processedSnapshotStorage.processedFrom([processedSnapshotHash])
       expect(processedSnapshotsInDB.size).toEqual(0)
     })
 
     it('should save the snapshot as processed when there are not entities to process and the stream is finished', async () => {
-      processedSnapshots.startStreamOf(processedSnapshot)
-      processedSnapshots.entityProcessedFrom(processedSnapshot)
-      processedSnapshots.entityProcessedFrom(processedSnapshot)
-      processedSnapshots.endStreamOf(processedSnapshot, 3)
-      processedSnapshots.entityProcessedFrom(processedSnapshot)
+      processedSnapshots.startStreamOf(processedSnapshotHash)
+      processedSnapshots.entityProcessedFrom(processedSnapshotHash)
+      processedSnapshots.entityProcessedFrom(processedSnapshotHash)
+      processedSnapshots.endStreamOf(processedSnapshotHash, 3)
+      processedSnapshots.entityProcessedFrom(processedSnapshotHash)
 
-      const processedSnapshotsInDB = await processedSnapshotStorage.processedFrom([processedSnapshot])
-      expect(processedSnapshotsInDB.has(processedSnapshot)).toBeTruthy()
+      const processedSnapshotsInDB = await processedSnapshotStorage.processedFrom([processedSnapshotHash])
+      expect(processedSnapshotsInDB.has(processedSnapshotHash)).toBeTruthy()
     })
   })
 })
