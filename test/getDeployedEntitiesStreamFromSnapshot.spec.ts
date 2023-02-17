a006c340285166ee052171620d7b28074af4fec0
Pedro Tamborindeguy <pftambo@gmail.com>
2023-02-17 13:49:43 -0300

    chore: remove deprecated test

--- test/getDeployedEntitiesStreamFromSnapshot.spec.ts
+++ test/getDeployedEntitiesStreamFromSnapshot.spec.ts
@@ -87,6 +87,13 @@ test('fetches a stream from snapshots deleting the downloaded file', ({ componen
 
 test('fetches a stream without deleting the downloaded file', ({ components, stubComponents }) => {
   const contentFolder = resolve('downloads')
+  const authChain = [
+    {
+      type: AuthLinkType.SIGNER,
+      payload: '0x3b21028719a4aca7ebee35b0157a6f1b0cf0d0c5',
+      signature: '',
+    },
+  ]
 
   it('prepares the endpoints', () => {
     // serve the snapshot file
@@ -139,60 +146,21 @@ test('fetches a stream without deleting the downloaded file', ({ components, stu
       r.push(deployment)
     }
 
+
+    expect(r).toEqual([
+      { entityType: 'profile', entityId: 'ba000000000000000000000000000000000000000000000000000000001', entityTimestamp: 1, authChain, pointers: ['0x1'], snapshotHash: downloadedSnapshotFile, servers },
+      { entityType: 'profile', entityId: 'ba000000000000000000000000000000000000000000000000000000002', entityTimestamp: 2, authChain, pointers: ['0x1'], snapshotHash: downloadedSnapshotFile, servers },
+      { entityType: 'profile', entityId: 'ba000000000000000000000000000000000000000000000000000000003', entityTimestamp: 3, authChain, pointers: ['0x1'], snapshotHash: downloadedSnapshotFile, servers },
+      { entityType: 'profile', entityId: 'ba000000000000000000000000000000000000000000000000000000004', entityTimestamp: 4, authChain, pointers: ['0x1'], snapshotHash: downloadedSnapshotFile, servers },
+      { entityType: 'profile', entityId: 'ba000000000000000000000000000000000000000000000000000000005', entityTimestamp: 5, authChain, pointers: ['0x1'], snapshotHash: downloadedSnapshotFile, servers },
+      { entityType: 'profile', entityId: 'ba000000000000000000000000000000000000000000000000000000006', entityTimestamp: 6, authChain, pointers: ['0x1'], snapshotHash: downloadedSnapshotFile, servers },
+      { entityType: 'profile', entityId: 'ba000000000000000000000000000000000000000000000000000000007', entityTimestamp: 7, authChain, pointers: ['0x1'], snapshotHash: downloadedSnapshotFile, servers },
+      { entityType: 'profile', entityId: 'ba000000000000000000000000000000000000000000000000000000008', entityTimestamp: 8, authChain, pointers: ['0x1'], snapshotHash: downloadedSnapshotFile, servers },
+      { entityType: 'profile', entityId: 'ba000000000000000000000000000000000000000000000000000000009', entityTimestamp: 9, authChain, pointers: ['0x1'], snapshotHash: downloadedSnapshotFile, servers },
+    ])
+
     // the downloaded file must not be deleted
     Sinon.assert.callCount(storage.delete, 0)
-  })
-})
-
-test('when successfully process a snapshot file', ({ components, stubComponents }) => {
-  const contentFolder = resolve('downloads')
-
-  it('prepares the endpoints', () => {
-    // serve the snapshot file
-    let downloadAttempts = 0
-    components.router.get(`/contents/${downloadedSnapshotFile}`, async () => {
-      if (downloadAttempts == 0) {
-        await sleep(100)
-        downloadAttempts++
-        return { status: 503 }
-      }
-
-      return {
-        body: createReadStream(`test/fixtures/${downloadedSnapshotFile}`),
-      }
-    })
-
-    try {
-      unlinkSync(resolve(contentFolder, downloadedSnapshotFile))
-    } catch { }
-  })
-
-  it('should call end of stream for all', async () => {
-    const { storage } = stubComponents
-    storage.storeStream.callThrough()
-    storage.retrieve.callThrough()
-    const servers = [await components.getBaseUrl()]
-    const r = []
-    const stream = getDeployedEntitiesStreamFromSnapshot(
-      {
-        metrics: components.metrics,
-        logs: components.logs,
-        storage: components.storage
-      },
-      {
-        requestRetryWaitTime: 0,
-        requestMaxRetries: 10,
-        tmpDownloadFolder: contentFolder
-      },
-      downloadedSnapshotFile,
-      new Set(servers)
-    )
-
-    for await (const deployment of stream) {
-      r.push(deployment)
-    }
-    // expect(endStreamOfSpy).toBeCalledTimes(1)
-    // expect(endStreamOfSpy).toBeCalledWith(downloadedSnapshotFile, 9)
   })
 })
 
